b4b96a0fded072387f033848ba435a83322e7272
Rémi FERMENT <ferment.remi.pro@gmail.com>
2026-02-24 15:03:53 +0100

    Add and achieve level 2.

--- index.php
+++ index.php
@@ -2,32 +2,61 @@
 //5 jarres, un serpent, le joueur choisis une jarre, random pour l'attribution du serpent et la clé
 
 $prompt = "";
+$winRate = 0;
 
 function initializeGame(): int
 {
-    return rand(1, 5.0);
+    return rand(1, 5);
 }
 
 initializeGame();
 
 // -----Game area-----
 while ($prompt != 'exit') {
-    $prompt = readline("Welcome to Jar Game ! Please select an option on the menu (play, exit) : ");
-    switch ($prompt) {
-        default:
+    $prompt = readline("Welcome to Jar Game 2 ! Please select an option on the menu (play, exit) : ");
+
+    $winRate = 0;
+
+    switch (strtolower(trim($prompt))) {
+
         case 'play':
-            $randomNumber = initializeGame();
-            echo "Let's started ! There are five jars here : [] [] [] [] []\n";
-            echo "All jar contains a key to go to the next level execpt one. One of them have a really venomous snake.\n";
-            $prompt = (int)readline("To select a jar, please enter a number between 1 or 5 :");
-            if ($prompt == $randomNumber) {
-                echo "Oh no ! There is a snake in jar $randomNumber !\n";
-                echo "Game Over.\n\n";
-            } else {
-                echo "Congratulation ! You win !\n\n";
+            echo "Let's started ! There are five jars here on the 3 next room : [] [] [] [] []\n";
+            echo "All jars contains a key to go to the next room except one. One of them has a really venomous snake.\n";
+            while ($winRate < 3) {
+                $prompt = trim(readline("You are in front jars. To select a jar, please enter a number between 1 and 5 :"));
+                $randomNumber = initializeGame();
+
+                if ($prompt == "" || !ctype_digit($prompt)) {
+                    echo "Invalid input ! Please enter a number.\n\n";
+                    continue;
+                }
+
+                $choice = (int)$prompt;
+
+                if ($choice < 1 || $choice > 5) {
+                    echo "\nInvalid input. Please make sure to enter a number between 1 and 5.\n\n";
+                    continue;
+                }
+
+                if ($choice == $randomNumber) {
+                    echo "Oh no ! There is a snake in jar $randomNumber !\n";
+                    echo "Game Over.\n\n";
+                    $winRate = 0;
+                    continue;
+                }
+                $winRate++;
+                $roomLeft = 3 - $winRate;
+                echo "Good Job ! You can enter to the new room ! $roomLeft room left.\n\n";
+            }
+            if ($winRate >= 3) {
+                echo "Congratulation ! You won 3 times!\n\n";
             }
             break;
+
         case 'exit':
-            return;
+            break;
+        default:
+            echo "Unknown option, please enter play or exit.\n";
+            break;
     }
 }
